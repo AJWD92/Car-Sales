fbba7caa632c621c24c2628b3689cc6e24603156
Aaron-Doran <wesley.gossett@gmail.com>
2020-02-04 14:31:43 -0500

    I have added a reducer pattren startung on the logic of the event handlers, reducers, & action creators.

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+const handleRemoveFeature = e => {
+  e.preventDefault();
+}
+
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={handleRemoveFeature} className="button">X</button>
       {props.feature.name}
     </li>
   );
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+const handleAddFeature = e => {
+  e.preventDefault();
+
+}
+
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={handleAddFeature} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
